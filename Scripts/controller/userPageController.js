59679b98aa87b37b1928236ff60b1793a74f52f6
Tom Liu <gt3240@me.com>
2016-06-13 16:44:05 -0700

    new

---
+++ Scripts/controller/userPageController.js
@@ -0,0 +1,74 @@
+﻿//UserPage Controller
+
+(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .controller('userPageController', userPageController);
+
+    userPageController.$inject = ['$scope', '$baseController', "$userPageService", "$signalRService"];
+
+    function userPageController(
+        $scope
+        , $baseController
+        , $userPageService
+        , $signalRService
+         ) {
+
+        var vm = this;
+        vm.headingInfo = "Angular 101";
+        vm.item = null;
+        vm.showNewEmployeeErrors = false;
+        vm.userId = $('#userId').val();
+        vm.isFollowing = $('#isFollowing').val();
+        vm.isLoggedIn = $('#isLoggedIn').val();
+
+
+        vm.$signalRService = $signalRService;
+        vm.$userPageService = $userPageService;
+        vm.$scope = $scope;
+
+        vm.receiveItems = _receiveItems;
+        vm.onAjaxErr = _onAjaxErr;
+        vm.init = _init;
+        vm.loadUser = loadUser;
+
+        $baseController.merge(vm, $baseController);
+
+        vm.notify = vm.$userPageService.getNotifier($scope);
+
+        _init();
+
+        function _init() {
+			vm.$userPageService.GetCurrentUserId(vm.receiveItems, vm.onAjaxErr);
+            vm.$systemEventService.broadcast("updateFollowers");
+            vm.$systemEventService.listen("testSuccess", loadUser);
+            vm.$systemEventService.listen("avatarSuccess", loadUser);
+        }
+
+        function _receiveItems(data) {  
+            vm.notify(function () {
+                vm.item = data.item;
+            });
+            console.log("layout user info", vm.item);
+            var userName = vm.item.firstName + " " + vm.item.lastName;
+
+            vm.$signalRService.startHub();
+            vm.$signalRService.Connect(userName, vm.item.id);
+        }
+		
+        vm.$signalRService.onConnected(function (message) {
+            console.log("connected ", message);
+        });
+
+        vm.$signalRService.dmReceived(function (message) {
+            $('#emailNotiDot').addClass('dot bg-danger');
+        });
+
+        function _onAjaxErr(response) {
+            console.log(response);
+        }
+
+     
+    }
+})();
---
+++ Scripts/service/signalRService.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .factory('$signalRService', SignalRServiceFactory);
+
+    SignalRServiceFactory.$inject = ['$baseService', '$sabio'];
+
+    function SignalRServiceFactory($baseService, $sabio) {
+
+        var svc = this;
+        svc = $baseService.merge(true, {}, svc, $baseService);
+
+        var $hub = $.connection.notificationHub;
+        var connection = null;
+        var signalR = {
+            startHub: function () {
+                console.log("started");
+                connection = $.connection.hub.start();
+            },
+            //////////////////// SERVER METHODS/////////////////
+            Connect: function (userName, userId) {
+                connection.done(function () {
+                    $hub.server.connect(userName, userId);
+                });
+            },
+            
+            ////////////////////// CLIENT METHODS////////////////////            
+            onConnected: function (callback) {
+                $hub.client.onConnected = callback;
+            },
+
+            dmReceived: function (callback) {
+                $hub.client.dmNotificationFromSever = callback;
+            }
+
+
+        }
+        return signalR;
+    }
+})();
